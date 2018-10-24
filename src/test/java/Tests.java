b054a40ad6925792b6a5165f45f093e88cb1585a
dzvarich <T@pckoe1>
2018-10-24 23:17:07 +0300

    Format change

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -57,7 +57,8 @@ public class Tests {
         for (String currency : bittrex
         ) {
             try {
-                System.out.println(currency + " Yesterday" + " = " + value1.get(currency) + "                "+ currency + " Today" + " = " + value2.get(currency));
+
+                System.out.println(currency + " Yesterday" + " = " + String.format("%.8f", value1.get(currency)) + "                "+ currency + " Today" + " = " + String.format("%.8f", value2.get(currency)));
                 System.out.println(currency + " = " + workingWithFinalData.percentChange(value1.get(currency), value2.get(currency)));
                 System.out.println();
             } catch (NullPointerException e) {
